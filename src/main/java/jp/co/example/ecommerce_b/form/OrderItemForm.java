1ccbfcc48a98298099ecb45d6aae924e0323576b
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2019-11-22 21:25:40 +0900

    OrderItemFormクラス作成

---
+++ src/main/java/jp/co/example/ecommerce_b/form/OrderItemForm.java
@@ -0,0 +1,122 @@
+package jp.co.example.ecommerce_b.form;
+
+import java.util.List;
+
+import jp.co.example.ecommerce_b.domain.Item;
+import jp.co.example.ecommerce_b.domain.OrderTopping;
+
+public class OrderItemForm {
+
+	private String id;
+	private String itemId;
+	private String orderId;
+	private String quantity;
+	private Character size;
+	private Item item;
+	private List<OrderTopping> orderToppingList;
+
+	@Override
+	public String toString() {
+		return "OrderItemForm [id=" + id + ", itemId=" + itemId + ", orderId=" + orderId + ", quantity=" + quantity
+				+ ", size=" + size + ", item=" + item + ", orderToppingList=" + orderToppingList + "]";
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the itemId
+	 */
+	public String getItemId() {
+		return itemId;
+	}
+
+	/**
+	 * @param itemId the itemId to set
+	 */
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+	/**
+	 * @return the orderId
+	 */
+	public String getOrderId() {
+		return orderId;
+	}
+
+	/**
+	 * @param orderId the orderId to set
+	 */
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	/**
+	 * @return the quantity
+	 */
+	public String getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+
+	/**
+	 * @return the size
+	 */
+	public Character getSize() {
+		return size;
+	}
+
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(Character size) {
+		this.size = size;
+	}
+
+	/**
+	 * @return the item
+	 */
+	public Item getItem() {
+		return item;
+	}
+
+	/**
+	 * @param item the item to set
+	 */
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	/**
+	 * @return the orderToppingList
+	 */
+	public List<OrderTopping> getOrderToppingList() {
+		return orderToppingList;
+	}
+
+	/**
+	 * @param orderToppingList the orderToppingList to set
+	 */
+	public void setOrderToppingList(List<OrderTopping> orderToppingList) {
+		this.orderToppingList = orderToppingList;
+	}
+
+}
