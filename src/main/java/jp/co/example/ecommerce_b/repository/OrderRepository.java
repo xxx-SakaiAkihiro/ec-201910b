4850e5ec74efba4c24fa9abef2967989f43bd4d8
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2019-11-23 22:56:35 +0900

    OrderRepository修正

--- src/main/java/jp/co/example/ecommerce_b/repository/OrderRepository.java
+++ src/main/java/jp/co/example/ecommerce_b/repository/OrderRepository.java
@@ -6,12 +6,23 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
+/**
+ * 注文するリポジトリ.
+ * 
+ * @author iidashuhei
+ *
+ */
 @Repository
 public class OrderRepository {
 
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 	
+	/**
+	 * 注文を挿入する.
+	 * 
+	 * @param order 注文した商品
+	 */
 	public void insertOrder(Order order) {
 		SqlParameterSource param = new BeanPropertySqlParameterSource(order);
 		String sql = "insert into orders(id,user_id,status,total_price,order_date,"
