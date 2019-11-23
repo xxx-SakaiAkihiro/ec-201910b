3aa310f3176417b3d39ce970e863a2f81b4fb1a3
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2019-11-23 22:20:02 +0900

    AddItemsService作成

--- src/main/java/jp/co/example/ecommerce_b/service/AddItemsService.java
+++ src/main/java/jp/co/example/ecommerce_b/service/AddItemsService.java
@@ -1,5 +1,45 @@
 package jp.co.example.ecommerce_b.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.example.ecommerce_b.domain.OrderItem;
+import jp.co.example.ecommerce_b.domain.OrderTopping;
+import jp.co.example.ecommerce_b.repository.OrderItemRepository;
+import jp.co.example.ecommerce_b.repository.OrderToppingRepository;
+
+/**
+ * 商品追加の機能.
+ * 
+ * @author iidashuhei
+ *
+ */
+@Service
+@Transactional
 public class AddItemsService {
 
+	@Autowired
+	public OrderItemRepository orderItemRepository;
+	
+	@Autowired
+	public OrderToppingRepository orderToppingRepository;
+	
+	/**
+	 * 注文された商品を挿入する.
+	 * 
+	 * @param orderItem　注文した商品
+	 */
+	public void insertOrderItem(OrderItem orderItem) {
+		orderItemRepository.insert(orderItem);;
+	}
+	/**
+	 * 注文されたトッピングを挿入する.
+	 * 
+	 * @param orderTopping 注文したトッピング
+	 */
+	public void insertOrderTopping(OrderTopping orderTopping) {
+		orderToppingRepository.insert(orderTopping);
+	}
+	
 }
