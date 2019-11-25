b1cbc982ac65ac86470517ca8e2aae613402a96e
TaroMoritsuka <pepperoni.4.eg@gmail.com>
2019-11-25 18:03:27 +0900

    SpringSecurityを導入し、ログイン機能とログアウトを実装

---
+++ src/main/java/jp/co/example/ecommerce_b/SecurityConfigure.java
@@ -0,0 +1,72 @@
+package jp.co.example.ecommerce_b;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigure extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	/**
+	 *静的リソースに対するセキュリティ設定を無視する
+	 */
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			.antMatchers("/css**"
+						,"/img**"
+						,"/js**"
+						,"/fonts**");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		//パスの書き方。コントローラのパスとメソッドのパス
+			.antMatchers("/showItem","/show"," ").permitAll()//全てのユーザでアクセス化
+			.anyRequest().authenticated();//それ以外のパスは認証必須
+		
+		http.formLogin()//ログインに関する設定
+			
+			.loginPage("/login/toLogin")//ログイン画面に遷移させるパス
+			.loginProcessingUrl("/login")//ログインhtmlのアクション属性のパスと一致させる.このアクションのボタンが押されたらSecurityがログイン認証を行う
+			.failureUrl("/?error=true")
+			.defaultSuccessUrl("/showOrderItem",true)//認証後第一引数のパスに遷移
+			.usernameParameter("mailAddress")
+			.passwordParameter("password");
+		
+		http.logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout**"))
+			.logoutSuccessUrl("/showItem")//ログアウト後に遷移させるパス
+			.deleteCookies("JSESSIONID")//ログアウト後Cookieに保存されているセッションIDを削除
+			.invalidateHttpSession(true);//true:ログアウト後セッションを無効にする
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService)
+			.passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/jp/co/example/ecommerce_b/controller/LoginController.java
@@ -0,0 +1,28 @@
+package jp.co.example.ecommerce_b.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * ログイン画面への操作をするコントローラー
+ * @author taro
+ *
+ */
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	
+	@RequestMapping("/toLogin")
+	public String toLogin(Model model,@RequestParam(required = false) String error) {
+		System.out.println("login error:" + error);
+		if(error != null) {
+			System.err.println("login failed");
+			model.addAttribute("errorMessage","メールアドレスまたはパスワードが不正です");
+		}
+		return "user/Login";
+	}
+	
+
+}
