dc1e643069166f383c8de92a05eb3c588a7b9177
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2019-11-25 17:02:17 +0900

    HTML変更

--- src/main/java/jp/co/example/ecommerce_b/repository/ToppingRepository.java
+++ src/main/java/jp/co/example/ecommerce_b/repository/ToppingRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-
+import org.springframework.stereotype.Repository;
 
 import jp.co.example.ecommerce_b.domain.Topping;
 
@@ -16,7 +16,12 @@ import jp.co.example.ecommerce_b.domain.Topping;
  * @author riho.ikeda
  *
  */
+@Repository
 public class ToppingRepository {
+	
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
 	/**
 	 * Toppingオブジェクトを生成するローマッパー.
 	 */
@@ -30,8 +35,6 @@ public class ToppingRepository {
 		return topping;
 	};
 
-	@Autowired
-	private NamedParameterJdbcTemplate template;
 
 	/**
 	 *トッピング情報を取得.
