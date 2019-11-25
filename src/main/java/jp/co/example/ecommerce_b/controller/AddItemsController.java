76da132e6f3b7fe5ff18fb0db0155962c49496cb
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2019-11-25 19:19:48 +0900

    AddItemController修正

--- src/main/java/jp/co/example/ecommerce_b/controller/AddItemsController.java
+++ src/main/java/jp/co/example/ecommerce_b/controller/AddItemsController.java
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.example.ecommerce_b.domain.OrderItem;
-import jp.co.example.ecommerce_b.domain.OrderTopping;
 import jp.co.example.ecommerce_b.form.OrderItemForm;
-import jp.co.example.ecommerce_b.form.OrderToppingForm;
 import jp.co.example.ecommerce_b.service.AddItemService;
 
 /**
@@ -44,23 +42,7 @@ public class AddItemsController {
 		orderItem.setOrderId((Integer.parseInt(orderItemForm.getOrderId())));
 		orderItem.setQuantity((Integer.parseInt(orderItemForm.getQuantity())));
 		session.setAttribute("orderItem", orderItem);
-		service.insertOrderItem(orderItem);
-		return "item_detail";
-	}
-	/**
-	 * トッピングを追加する.
-	 * 
-	 * @param orderToppingForm トッピング注文フォーム
-	 * @return 商品詳細画面
-	 */
-	@RequestMapping("/addToppings")
-	public String addToppings(OrderToppingForm orderToppingForm) {
-		OrderTopping orderTopping = new OrderTopping();
-		BeanUtils.copyProperties(orderToppingForm, orderTopping);
-		orderTopping.setId((Integer.parseInt(orderToppingForm.getId())));
-		orderTopping.setToppingId((Integer.parseInt(orderToppingForm.getToppingId())));
-		orderTopping.setOrderItemId((Integer.parseInt(orderToppingForm.getOrderItemId())));
-		service.insertOrderTopping(orderTopping);
+		service.addItem(orderItem);
 		return "item_detail";
 	}
 }
