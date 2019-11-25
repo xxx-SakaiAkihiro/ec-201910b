2fb76bd739dcf9dfcd91938f478c350df4df22cd
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2019-11-25 20:04:06 +0900

    PurchaseService修正

--- src/main/java/jp/co/example/ecommerce_b/repository/OrderRepository.java
+++ src/main/java/jp/co/example/ecommerce_b/repository/OrderRepository.java
@@ -44,7 +44,7 @@ public class OrderRepository {
 	 * 
 	 * @param order 注文した商品
 	 */
-	public void insertOrder(Order order) {
+	public void insert(Order order) {
 		SqlParameterSource param = new BeanPropertySqlParameterSource(order);
 		String sql = "insert into orders(id,user_id,status,total_price,order_date,"
 				+ "destination_name,destination_email,destination_zipcode,"
