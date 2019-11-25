f949eeabd915772110da11b48183990772abd3e1
ikedariho <riho.ikeda@rakus-partners.co.jp>
2019-11-25 20:06:49 +0900

    注文画面確認

---
+++ src/main/java/jp/co/example/ecommerce_b/controller/ShowOrderedItemContoroller.java
@@ -0,0 +1,18 @@
+package jp.co.example.ecommerce_b.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 注文確認画面を表示するコントローラ.
+ * 
+ * @author riho.ikeda
+ *
+ */
+@Controller
+@RequestMapping("/ShowOrderItem")
+public class ShowOrderedItemContoroller {
+	
+	
+
+}
