2c58eeace5ccb80471045b2916ae94a36bc5e03d
TaroMoritsuka <pepperoni.4.eg@gmail.com>
2019-11-26 19:25:29 +0900

    mailSenderを追加

--- src/main/java/jp/co/example/ecommerce_b/controller/PurchaseController.java
+++ src/main/java/jp/co/example/ecommerce_b/controller/PurchaseController.java
@@ -1,7 +1,5 @@
 package jp.co.example.ecommerce_b.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,5 +46,10 @@ public class PurchaseController {
 		}
 		purchaseService.purchase(order);
 		return "order_finished";
+	}
+	
+	@RequestMapping("/mail")
+	public void sendMail() {
+		purchaseService.sendMail();
 	}
 }
