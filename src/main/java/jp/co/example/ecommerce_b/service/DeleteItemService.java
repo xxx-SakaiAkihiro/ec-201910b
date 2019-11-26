13329b53a6237ce58357f05598b0122c814851a5
ikedariho <riho.ikeda@rakus-partners.co.jp>
2019-11-26 11:00:58 +0900

    カートの商品削除　Sql

---
+++ src/main/java/jp/co/example/ecommerce_b/service/DeleteItemService.java
@@ -0,0 +1,31 @@
+package jp.co.example.ecommerce_b.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.example.ecommerce_b.repository.OrderItemRepository;
+
+/**
+ * カートの商品を削除するサービス.
+ * 
+ * @author riho.ikeda
+ *
+ */
+@Service
+@Transactional
+public class DeleteItemService {
+
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+
+	/**
+	 * カートの商品を削除するサービス.
+	 * 
+	 * @param orderItemId ID
+	 */
+	public void deleteItem(Integer orderItemId) {
+		orderItemRepository.deleteById(orderItemId);
+	}
+
+}
