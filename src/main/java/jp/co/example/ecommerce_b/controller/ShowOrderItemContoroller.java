f7e747e93078e16e7b40c7f23108011e0d7afbd1
SakaiAkihiro <akihiro.sakai@rakus-partners.co.jp>
2019-11-28 15:26:36 +0900

    ショッピングカート内が0件の場合を作成

--- src/main/java/jp/co/example/ecommerce_b/controller/ShowOrderItemContoroller.java
+++ src/main/java/jp/co/example/ecommerce_b/controller/ShowOrderItemContoroller.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.example.ecommerce_b.domain.LoginUser;
@@ -27,17 +28,17 @@ public class ShowOrderItemContoroller {
 
 	@Autowired
 	private ShowOrderItemService showOrderItemService;
-	
+
 	@Autowired
 	private HttpSession session;
-	
+
 	@Autowired
 	private CountInCartService countInCartService;
-	
+
 	/**
 	 * ショッピングカートの中身を表示する.
 	 * 
-	 * @param model モデル
+	 * @param model     モデル
 	 * @param loginUser ユーザーID
 	 * @return 「ショッピングカート画面」を表示
 	 */
@@ -50,11 +51,22 @@ public class ShowOrderItemContoroller {
 			userId = loginUser.getUser().getId();
 		}
 		List<Order> orderList = showOrderItemService.findByUserIdAndStatus(userId);
+		//ショッピングカートの中身が0件か1件以上の場合
+		Order order = new Order();
+		order = orderList.get(0);
+		//ショッピングカートの中身が0件
+		if (CollectionUtils.isEmpty(order.getOrderItemList())) {
+			String str = "カートに商品がありません";
+			model.addAttribute("str", str);
+		//ショッピングカートの中身が1件以上
+		} else {
+			model.addAttribute("order", order);
+		}
 		model.addAttribute("orderList", orderList);
-		
+		// 現在のショッピングカート内の数量表示用
 		Integer countInCart = countInCartService.countInCart(loginUser);
-		model.addAttribute("countInCart",countInCart);
+		model.addAttribute("countInCart", countInCart);
 		return "cart_list";
 	}
-	
+
 }
